fccab48245dd8c752d41c10699a0064e7d0c5ba6
Rahul Solanki <rahul.solanki@infinitylabs.in>
2018-04-25 11:16:02 +0530

    two component added sample and example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Goa';
+   a = 10;
+
+   constructor(){
+     console.log(this.a);
+   }  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SampleComponent } from './sample/sample.component';
+import { ExampleComponent } from './example/example.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule
